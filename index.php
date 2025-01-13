7d1e9d92585ef73754350ff65e53516f3933c435
ArtCode9 <alirezatavanmand91@gmail.com>
2025-01-12 22:12:57 -0800

    intro php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+      echo"I love pizza <br>";
+      echo"It's really good";
+      // comment single line
+      /* 
+         comment multi line
+      */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+         <br>
+         <button>Order pizza</button>  
+</body>
+</html>
