344796c0a465c59b0e03f46abb72aff27109a20e
ArtCode9 <alirezatavanmand91@gmail.com>
2025-01-16 20:17:26 -0800

    isset

--- 12_associative_array.php
+++ 12_associative_array.php
@@ -6,7 +6,11 @@
    <title>Associative Array</title>
 </head>
 <body>
-
+         <form action="12_associative_array.php" method="post">
+               <label>Enter a country:</label>
+               <input type="text" name="country">
+               <input type="submit">
+         </form>
 </body>
 </html>
 <?php 
@@ -14,7 +18,15 @@
                            "Japan" => "Kyoto",
                            "South Korea" => "Seoul",
                            "India" => "New Delhi");
+         
+                           
+         $capital = $capitals[$_POST["country"]];
+         echo " THE capital is" . $capital . "<br>";
+
+
+         echo"=====================<br>";
          echo $capitals["Japan"] . "<br>";
+         
          
          // change one of them use key
          $capitals["USA"] = "Las Vegas"; 
@@ -25,13 +37,36 @@
          // shift function remove the first
          array_shift($capitals);
          // need all of the keys in this array it also return new array 
-         array_keys($capitals);
-
+         $cat = array_keys($capitals);
+         // need all of values in this array 
+         $val = array_values($capitals);
+         // we can flip keys and the values
+         $fliper = array_flip($capitals);
+         //  if need to reverse the order
+         $capcap = array_reverse($capitals);
+         // if need amount of key value pairs 
+         echo count($capitals) . "<br>";
+         echo count($cat) . "<br>";
+         echo count($val) . "<br>";
+ 
 
          foreach($capitals as $key => $value){
             echo"{$key} is = {$value}<br>";
          };
-
-
-
+         echo"=====================<br>";
+         foreach($cat as $key){
+            echo"{$key} <br>";
+         };
+         echo"=====================<br>";
+         foreach($val as $key){
+            echo"{$key} <br>";
+         };
+         echo"=====================<br>";
+         foreach($fliper as $key => $value){
+            echo"{$key} = {$value}<br>";
+         }; 
+         echo"=====================<br>";
+         foreach($capcap as $key => $value){
+            echo"{$key} is = {$value}<br>";
+         };
 ?>
---
+++ 13_issetEmpty.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>isset() empty()</title>
+</head>
+<body>
+         <form action="13_issetEmpty.php" method="post">
+            <label>Username:</label>
+            <input type="text" name="username"><br>
+            <label>Password:</label>
+            <input type="password" name="password"><br>
+            <input type="submit" value="Log in" name="login"><br>
+         </form>   
+</body>
+</html>
+<?php
+         foreach($_POST as $key => $value){
+               echo"{$key} = {$value} <br>";
+         }
+
+
+
+
+         // isset() = returns true if a variable is declared and not null
+         // empty() = returns true if a variable is not declared , false , null, ""
+
+         $username = true;
+         $nickname = "coder";
+
+         if(isset($username)){
+            echo "This username is set<br>";
+         }else {
+            echo "set a username for yourself<br>";
+         }
+
+         if(empty($nickname)){
+            echo"This variable is empty";
+         }else{
+            echo"This variable is NOT empty";
+         }
+
+?>
