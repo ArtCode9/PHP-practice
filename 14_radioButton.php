733b2a4bf05b64b0203f0de99363cc720b5ffdd5
ArtCode9 <alirezatavanmand91@gmail.com>
2025-01-16 21:56:54 -0800

    radio

--- 14_radioButton.php
+++ 14_radioButton.php
@@ -3,13 +3,18 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Document</title>
+   <title>Radio button</title>
 </head>
 <body>
-   
+            <form action="14_radioButton.php" method="post">
+               <input type="radio" name="credit_card" value="Visa">Visa<br>
+               <input type="radio" name="credit_card" value="Mastercard">Mastercard<br>
+               <input type="radio" name="credit_card" value="American Express">American Express<br>
+               <input type="submit" name="confirm" value="confirm">
+            </form>
 </body>
 </html>
 <?php
-
+         
 
 ?>
