08331cc9076889c49c85b28c21d5963a72023578
ArtCode9 <alirezatavanmand91@gmail.com>
2025-01-16 23:44:12 -0800

    add

--- 14_radioButton.php
+++ 14_radioButton.php
@@ -12,9 +12,37 @@
                <input type="radio" name="credit_card" value="American Express">American Express<br>
                <input type="submit" name="confirm" value="confirm">
             </form>
+            <hr>
+            <form action="14_radioButton.php" method="post">
+               <input type="radio" name="credit_card" value="Visa">Visa<br>
+               <input type="radio" name="credit_card" value="Mastercard">Mastercard<br>
+               <input type="radio" name="credit_card" value="American Express">American Express<br>
+               <input type="submit" name="confirm" value="confirm">
+            </form>
 </body>
 </html>
 <?php
-         
+         if(isset($_POST["confirm"])){
+
+            $credit_card = null;
+               
+            if(isset($_POST["credit_card"])){
+               $credit_card = $_POST["credit_card"];
+               // echo $credit_card;    
+            }/* else{
+               echo"Please make a selection!";
+            } */
+
+            if($credit_card == "Visa"){
+               echo"You selected Visa";
+            }elseif($credit_card == "Mastercard"){
+               echo"You selected Mastercard";
+            }elseif($credit_card == "American Express"){
+               echo"You selected American Express";
+            }else{
+               echo"Please make a selection!";
+            }
+            
+         }
 
 ?>
