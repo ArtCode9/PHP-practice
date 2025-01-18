02e407b80b142309df1d092eedf723ecb1dd8243
ArtCode9 <alirezatavanmand91@gmail.com>
2025-01-18 11:27:10 -0800

    mysqli

--- 23_mysql.php
+++ 23_mysql.php
@@ -1,7 +1,10 @@
+<!-- file belong to this section ==> database.php -->
+
 <?php 
       // 1. mysqli extension
       // 2. PDO (PHP Data Objects)
       
+      // how to connect to a mysql server in  php??
  
 
 ?>
