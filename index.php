0b1e9a05620cd81e46ec717e56bf7d3d1431eeaf
ArtCode9 <alirezatavanmand91@gmail.com>
2025-01-23 17:42:33 -0800

    part1

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
-      echo "hello this is new start so lets go"; 
+      $num =  23;
+
+      $name = "Hide";
+
+      var_dump($num);
+      var_dump($name);
+
 ?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
+<?php 
+
+?>
