ad5a647b20ae9262325cb84bf6ce002854262669
ArtCode9 <alirezatavanmand91@gmail.com>
2025-01-24 22:43:44 -0800

    operator

--- operators.php
+++ operators.php
@@ -62,5 +62,76 @@
       echo "x" <=> "y";
       echo "<br>";
       echo "y" <=> "x";
+      echo'<hr>';
+      // logical operator
+      
+      // && and operator
+      $q = 10;
+      $w = 20;
+      $e = 10;
+
+      if($q == 10 && $w == 20 && $e == 11){
+            echo'true';
+      }else{
+            echo'false';
+      }
+      echo'<hr>';
+
+      // || or operator 
+      if($q == 10 or $w == 20 || $e == 11){
+            echo'🤯true';
+      }else{
+            echo'🤯false';
+      }
+      echo'<hr>';
+
+      // xor
+      if($q == 10 xor $w == 21){
+            echo'🤑true';
+      }else{
+            echo'🤑false';
+      }
+
+      echo'<hr>';
+
+      // !=  
+
+      if($q !== 12){
+            echo'🤢true';
+      }else{
+            echo'🤢false';
+      }
+
+      echo'<hr>';
+
+      $x_x = array('r' => 'red', 'b' => 'blue', 'g' => 'green');
+      $x_x2 = array('r' => 'red', 'b' => 'blue', 'g' => 'green');
+      $o_p = array('o' => 'orange', 'y' => 'yellow', 'p' => 'pink');
+
+      print_r($x_x);
+      echo'<br>';
+      print_r($o_p);
+      echo'<hr>';
+
+      // union operator  +
+      $o_o = $x_x + $o_p;
+      print_r($o_o);
+
+      echo'<hr>';
+      // equality operator both array not have equal element
+      $y_y = $x_x == $o_p;
+      var_dump($y_y);
+
+      echo'<hr>';
+      echo'<hr>';
+      // === identity
+      var_dump($x_x === $x_x2);
+
+      echo'<hr>';
+      echo'<hr>';
+      // <> inequality  the behaiver similar to !=
+      var_dump($x_x <> $x_x2);
+      var_dump($x_x !== $x_x2);
+
 
 ?>
