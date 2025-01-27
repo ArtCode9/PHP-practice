af827ef9a22620f4662e18e6c57b1a6a6850fab9
ArtCode9 <alirezatavanmand91@gmail.com>
2025-01-26 22:21:15 -0800

    elseif

--- condition2.php
+++ condition2.php
@@ -2,7 +2,7 @@
 
    $user = array(
       "ali" => [
-                 "role" => "author",
+                 "role" => "admin",
                  "age" => 16,
                  "email" => "ali@gmail.com"
                ],
@@ -17,6 +17,7 @@
                   "email" => "sara@gmail.com"
                 ]
    );
+
 $usersCo = json_decode(json_encode($user));
 var_dump($usersCo);
 echo"<hr>";
@@ -24,13 +25,23 @@ echo"<hr>";
 if($usersCo->ali->role == "author" && $usersCo->ali->age == 16){
       echo"ali is verify to be at team<br>";
       if($usersCo->sara->age >= $usersCo->ali->age && $usersCo->sara->role == "editor"){
-         echo"you are in team with sara";
+         echo"you are in team with sara<br>";
       }else{
-         echo"waiting for manager to manage your duty";
+         echo"waiting for manager to manage your duty<br>";
       }
-}else{
-   echo"review your condition dear {$user["ali"]["email"]}";
+}elseif ($usersCo->ali->role == "admin" && $usersCo->ali->age >= 16){
+   echo"dear {$user["ali"]["email"]} you are qualify to be an admin<br>";
+   if($usersCo->sara->role == $usersCo->ali->role){
+      echo"ali and sara both the admin of the saling page<br>";
+   }else if($usersCo->erfan->role == $usersCo->ali->role){
+      echo"ali and erfan both the admin of the saling page<br>";
+   }else{
+      echo"you are not in admin with anybody";
+   }
 }
+echo"<hr>";
+
+
 
 
 ?>
