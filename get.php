7701c4d7a78bc2e9eb000d66d24d177439b55e67
ArtCode9 <alirezatavanmand91@gmail.com>
2025-01-28 12:22:50 -0800

    POST

--- get.php
+++ get.php
@@ -3,7 +3,7 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Get And Post</title>
+   <title>Get Method</title>
 </head>
 <body>
 
---
+++ post.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>POST Method</title>
+</head>
+<body>
+         <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
+                  
+         </form>  
+</body>
+</html>
+<?php 
+
+
+?>
