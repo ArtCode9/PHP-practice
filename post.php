79b2bc6847ff6b9c69ad3eba6b2dd9359ecd8f1f
ArtCode9 <alirezatavanmand91@gmail.com>
2025-01-28 12:46:04 -0800

    post

--- post.php
+++ post.php
@@ -7,11 +7,24 @@
 </head>
 <body>
          <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
-                  
+                  <label>Name:</label><br>
+                  <input type="text" name="Fname" placeholder="Name"><br>
+                  <hr>
+                  <label>Age:</label><br>
+                  <input type="text" name="age" placeholder="Your Age"><br>
+                  <hr>
+                  <input type="submit" value="SEND" name="send">
          </form>  
 </body>
 </html>
-<?php 
-
-
+<?php
+      
+      if(isset($_POST["send"])){
+         if(!empty($_POST["Fname"]) && !empty($_POST["age"])){
+            echo "Welcome " . $_POST["Fname"]. "<br>";
+            echo "You are " . $_POST["age"] . " old";
+         } else{
+               echo"please fill the form";
+         }
+      };
 ?>
