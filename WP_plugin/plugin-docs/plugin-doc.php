99183459a24afcbb9a76b67549b8d2d8207f6f5d
artcode9 <alirezatavanmand91@gmail.com>
2025-03-06 10:12:47 +0330

    plugin-docs

---
+++ WP_plugin/plugin-docs/plugin-doc.php
@@ -0,0 +1,17 @@
+<?php
+/* 
+   Plugin name: Plugin_doc
+   Plugin URI: www.example.com
+   Description: This plugin is  created with the studying documentation of wordpress
+   Version: 1.10.4
+   Author: ART_CODE9
+   Author URI: www.example.com
+*/
+
+
+
+
+
+
+?>
+
