f8dbcd2b03e6f046821a487341dbe6e3f37b1584
ArtCode9 <alirezatavanmand91@gmail.com>
2025-04-01 14:26:04 -0700

    this ismake full understanding of php

---
+++ PHPNET.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "This is Full study to advance PHP from php.net doc";
+
+?>
+
+<hr style="border:2px solid orange">
+
+<?php echo "So this is a way to become expert for wordpress and php"; ?> 
